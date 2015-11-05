c61f927d5535a80cf2f042a0920c7410ceeb46a6
micwallace <micwallace@gmx.com>
2015-11-05 16:59:35 +1100

    ported extension to chrome.

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # VineVolumeFixer
-A simple addon for firefox that sets vine.co videos to a sane level.
+A simple addon for firefox & chrome that sets vine.co videos to a sane level.
 
 Vine.co videos play way too loud and don't have volume control.
 
 This add-on adjusts vine video volume to a sane level of your choosing.
 It also (optionally) adds a volume control & un-mutes the video when playback starts.
 
-Install in firefox:
+Install for firefox:
+
 https://addons.mozilla.org/en-US/firefox/addon/vinevolumefixer/
+
+Install for Chrome:
+
+https://chrome.google.com/webstore/detail/vinevolumefixer/comphldkanjbmkobkilkkgpmjghkbhlm
---
+++ chrome/pagemod.js
@@ -0,0 +1,129 @@
+// VineVolumeFixer
+function setCurrentVolume(video){
+    video.volume = currentVolume;
+    //console.log("VineVolumeFixer: volume adjusted: "+video.volume);
+}
+
+function unmuteVideo(video){
+    video.muted = false;
+    // unmute ui, only needed for embedded player
+    if (isEmbed){
+        document.getElementsByClassName("mute-button")[1].classList.remove("muted");
+    }
+}
+
+function addVolumeControl(video){
+    if (!self.options.showui)
+        return;
+
+    // element to attach volume control to
+    var target = isEmbed?document.getElementsByClassName("actions")[0]:video.parentElement.parentElement;
+
+    if (target.querySelector(".vine_volume_fix_control")!==null) // prevent multiple controls
+        return;
+
+    var volumeBar = document.createElement("input");
+    volumeBar.className = "vine_volume_fix_control";
+    volumeBar.type = "range";
+    volumeBar.min = 0;
+    volumeBar.max = 1;
+    volumeBar.step = 0.01;
+    volumeBar.value = self.options.defaultvolume;
+
+    volumeBar.addEventListener("change", function(e) {
+        currentVolume = volumeBar.value;
+        setCurrentVolume(video);
+        unmuteVideo(video);
+        e.stopPropagation();
+    });
+
+    volumeBar.style.width = "80px";
+    volumeBar.style.zIndex = 3;
+    volumeBar.style.background = "none";
+    volumeBar.style.cursor = "pointer";
+
+    // embedded vine player controls are positioned differently
+    if (isEmbed){
+        volumeBar.style.opacity = 1;
+        volumeBar.style.bottom = "2px";
+    } else {
+        volumeBar.style.position = "absolute";
+        volumeBar.style.top = "4px";
+        volumeBar.style.left = "35px";
+        volumeBar.style.opacity = 0;
+        volumeBar.style.transition = "opacity 0.1s ease 0s";
+        // hover for vine website player
+        var style = document.createElement('style');
+        var css = '.vine-player:hover .vine_volume_fix_control{ opacity:1 !important; }';
+        style.appendChild(document.createTextNode(css));
+        document.getElementsByTagName('head')[0].appendChild(style);
+    }
+
+    target.appendChild(volumeBar);
+    //console.log("VineVolumeFixer: volume meter added");
+}
+
+function init() {
+
+    document.addEventListener('loadeddata', function (e) {
+        //console.log("VineVolumeFixer: onloadeddata");
+        setCurrentVolume(e.target);
+        addVolumeControl(e.target);
+        if (self.options.unmute) {
+            if (isEmbed) {
+                setTimeout(function () {
+                    unmuteVideo(e.target);
+                }, 500);
+            } else {
+                unmuteVideo(e.target);
+            }
+        }
+    }, true);
+
+// embedded player mutes on each play
+    if (isEmbed)
+        document.addEventListener('play', function (e) {
+            //console.log("VineVolumeFixer: onplay");
+            if (self.options.unmute) {
+                setTimeout(function () {
+                    unmuteVideo(e.target);
+                }, 500);
+            }
+        }, true);
+
+    document.addEventListener('volumechange', function (e) {
+        //console.log("VineVolumeFixer: onvolumechange");
+        setCurrentVolume(e.target);
+    }, true);
+
+    var videos = document.getElementsByTagName("video");
+    for (var i in videos) {
+        if (videos.hasOwnProperty(i)) {
+            if (self.options.showui)
+                addVolumeControl(videos[i]);
+            if (self.options.unmute)
+                unmuteVideo(videos[i]);
+        }
+    }
+}
+
+var currentVolume = 15;
+var isEmbed = document.getElementsByClassName("embed").length>0;
+
+chrome.storage.sync.get({
+    defaultvolume: 15,
+    showui: true,
+    unmute: true
+}, function(items) {
+    // retrieve preferences before initialization
+    var defaultvolume = items.defaultvolume;
+    defaultvolume = defaultvolume>100?100:(defaultvolume<0?0:defaultvolume);
+    defaultvolume = Number((defaultvolume/100).toFixed(2));
+    self.options = {
+        defaultvolume: defaultvolume,
+        showui: items.showui,
+        unmute: items.unmute
+    };
+    currentVolume = defaultvolume;
+    init();
+});
